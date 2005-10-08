bce3bcb69bf8d0917cec962f36322710e27772ac
cultpenguin <none@none>
2005-10-08 19:48:49 +0000

    *** empty log message ***

--- GUI/gui.py
+++ GUI/gui.py
@@ -19,6 +19,15 @@
 import pygtk
 pygtk.require('2.0')
 
+
+import matplotlib 
+matplotlib.use('GTK') 
+from matplotlib.figure import Figure 
+from matplotlib.axes import Subplot 
+from matplotlib.backends.backend_gtk import FigureCanvasGTK, NavigationToolbar 
+from matplotlib.numerix import arange, sin, pi 
+
+
 import gtk, gtk.glade
 
 import segypy
@@ -33,6 +42,52 @@ class SimpleTest:
 		print 'foo'
 		self.segy = segypy.readSegy('../data_4byteINT.segy')
 		print 'foo2'
+
+	def on_open1_activate(self, button):
+		dialog = gtk.FileChooserDialog("Open..",
+			None,
+			gtk.FILE_CHOOSER_ACTION_OPEN,
+			(gtk.STOCK_CANCEL, gtk.RESPONSE_CANCEL,
+			gtk.STOCK_OPEN, gtk.RESPONSE_OK))
+		dialog.set_default_response(gtk.RESPONSE_OK)
+
+		filter = gtk.FileFilter()
+		filter.set_name("All files")
+		filter.add_pattern("*")
+		
+		dialog.add_filter(filter)
+		filter = gtk.FileFilter()
+		filter.set_name("SEG-Y files")
+		filter.add_pattern("*.segy")
+		filter.add_pattern("*.sgy")
+		filter.add_pattern("*.seg-y")
+		dialog.add_filter(filter)
+		filter = gtk.FileFilter()
+		filter.set_name("SU files")
+		filter.add_pattern("*.su")
+		dialog.add_filter(filter)
+		response = dialog.run()
+		if response == gtk.RESPONSE_OK:
+			print dialog.get_filename(), 'selected'
+			filename  = dialog.get_filename()
+			dialog.destroy()
+			self.segy = segypy.readSegy(filename)
+		
+			self.figure = Figure(figsize=(6,4), dpi=72) 
+			self.axis = self.figure.add_subplot(111) 
+			self.axis.set_xlabel('Yepper') 
+			self.axis.set_ylabel('Flabber') 
+			self.axis.set_title('An Empty Graph') 
+			self.axis.grid(True) 
+			self.canvas = FigureCanvasGTK(self.figure) # a gtk.DrawingArea 
+			self.canvas.show() 
+#			self.graphview = self.wTree.get_widget("vbox1") 
+#			self.graphview.pack_start(self.canvas, True, True)
+		
+		
+		elif response == gtk.RESPONSE_CANCEL:
+			print 'Closed, no files selected'
+			dialog.destroy()
 
 test = SimpleTest()
 gtk.main()

e938be8507b7d83d3711c92345ad367cf3dcee0b
Robert Smallshire <robert@smallshire.org.uk>
2015-02-26 20:23:31 +0100

    Fix trove classifier

--- setup.py
+++ setup.py
@@ -60,7 +60,8 @@ setup(
 
         # Indicate who your project is intended for
         'Intended Audience :: Developers',
-        'Topic :: Software Development :: Build Tools',
+        'Topic :: Scientific/Engineering',
+        'Topic :: Software Development :: Libraries',
 
         # Pick your license as you wish (should match "license" above)
         'License :: OSI Approved :: MIT License',

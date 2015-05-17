5e33d2731cee3e9b5a340d4e419b7ea802b4603f
Robert Smallshire <robert@smallshire.org.uk>
2015-05-17 13:13:17 +0200

    Updates trove classifier for license.

--- setup.py
+++ setup.py
@@ -64,7 +64,7 @@ setup(
         'Topic :: Software Development :: Libraries',
 
         # Pick your license as you wish (should match "license" above)
-        'License :: OSI Approved :: MIT License',
+        'License :: OSI Approved :: GNU Affero General Public License v3',
 
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.

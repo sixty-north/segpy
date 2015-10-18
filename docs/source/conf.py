2e394291318f8bb9aed472f99cc9be7e8a78b3a7
Robert Smallshire <robert@smallshire.org.uk>
2015-10-18 15:44:46 +0200

    Corrects company name

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -52,7 +52,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'Segpy'
-copyright = '2015, Robert Smallshire'
+copyright = '2015, Sixty North'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -211,7 +211,7 @@ latex_elements = {
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
   ('index', 'Segpy.tex', 'Segpy Documentation',
-   'Robert Smallshire', 'manual'),
+   'Sixty North', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
@@ -241,7 +241,7 @@ latex_documents = [
 # (source start file, name, description, authors, manual section).
 man_pages = [
     ('index', 'segpy', 'Segpy Documentation',
-     ['Robert Smallshire'], 1)
+     ['Sixty North'], 1)
 ]
 
 # If true, show URL addresses after external links.
@@ -255,7 +255,7 @@ man_pages = [
 #  dir menu entry, description, category)
 texinfo_documents = [
   ('index', 'Segpy', 'Segpy Documentation',
-   'Robert Smallshire', 'Segpy', 'One line description of project.',
+   'Sixty North', 'Segpy', 'One line description of project.',
    'Miscellaneous'),
 ]
 

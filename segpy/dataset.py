a022627ad8cd9831c1ce4afcd57d0dbb362484d3
Robert Smallshire <robert@smallshire.org.uk>
2017-05-12 15:30:24 +0200

    Adds a DelegatingDataset which simply forwards to an underlying dataset. It's a convenience base-class for subclassing.

--- segpy/dataset.py
+++ segpy/dataset.py
@@ -1,5 +1,7 @@
 from abc import ABCMeta, abstractmethod
 
+from segpy.datatypes import DATA_SAMPLE_FORMAT_TO_SEG_Y_TYPE, SEG_Y_TYPE_DESCRIPTION
+
 
 class Dataset(metaclass=ABCMeta):
 
@@ -29,7 +31,6 @@ class Dataset(metaclass=ABCMeta):
         """
         raise NotImplementedError
 
-    @property
     @abstractmethod
     def trace_indexes(self):
         """An iterator over zero-based trace_samples indexes.
@@ -70,3 +71,62 @@ class Dataset(metaclass=ABCMeta):
         """
         raise NotImplementedError
 
+    @property
+    def data_sample_format(self):
+        """The data type of the samples in machine-readable form. One of the values from datatypes.DATA_SAMPLE_FORMAT.
+        """
+        return DATA_SAMPLE_FORMAT_TO_SEG_Y_TYPE[self.binary_reel_header.data_sample_format]
+
+    @property
+    def data_sample_format_description(self):
+        """A descriptive human-readable description of the data sample format
+        """
+        return SEG_Y_TYPE_DESCRIPTION[self.data_sample_format]
+
+
+class DelegatingDataset(Dataset):
+    """A Dataset which by default forwards to a source Dataset.
+
+    This base class is useful if you only want to override a
+    few methods to perform transformations.
+    """
+
+    def __init__(self, source_dataset):
+        self._source = source_dataset
+
+    @property
+    def source(self):
+        return self._source
+
+    def trace_samples(self, trace_index, start=None, stop=None):
+        return self._source.trace_samples(trace_index, start, stop)
+
+    @property
+    def textual_reel_header(self):
+        return self._source.textual_reel_header
+
+    def trace_header(self, trace_index):
+        return self._source.trace_header(trace_index)
+
+    @property
+    def binary_reel_header(self):
+        return self._source.binary_reel_header
+
+    def trace_indexes(self):
+        return self._source.trace_indexes()
+
+    @property
+    def dimensionality(self):
+        return self._source.dimensionality
+
+    @property
+    def extended_textual_header(self):
+        return self._source.extended_textual_header
+
+    @property
+    def encoding(self):
+        return self._source.encoding
+
+    @property
+    def endian(self):
+        return self._source.endian
